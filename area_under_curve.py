f1e669333ca44f3e346dc486c55926d1913a7ac2
al4asus <105155448+al4asus@users.noreply.github.com>
2025-02-20 23:12:14 +0300

    update

--- area_under_curve.py
+++ area_under_curve.py
@@ -17,7 +17,8 @@ from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
 from sklearn.model_selection import KFold, cross_val_score
-import joblib
+
+import pickle
 import os
 import shap
 
@@ -61,10 +62,19 @@ df['antifungalkullanimi'] = df['antifungalkullanimi'].astype(int)
 df = pd.get_dummies(df, columns=['ilac', 'gebelik_haftasi_gruplu', 'tani_gruplu', 'dogum_agirligi_gruplu', 'cinsiyeti',
                                  'gebelik_tipi_gruplu', 'dogumsekli', 'nefrotoksikilacne', 'antifungalkullanimi', 'tanisi'], drop_first=True)
 
-
+"""
+df['antifungalkullanimi'] = df['antifungalkullanimi'].map({
+    0.0: 'Yok',
+    1.0: 'Kantiseptik Kullanmış',
+    2.0: 'Amfetorisin Kullanmış',
+    3.0: 'Flukazanol Kullanmış',
+    4.0: 'Mikafungin Kullanmış',
+    5.0: 'Kaspafungin Kullanmış'
+})
+"""
 # NaN değerlere sahip satırları atla
-df = df.fillna(method="ffill")
 
+#hocaya sor
 
 def transform_difference(dataframe, column_name_1, column_name_2, column_name):
     df[column_name] = df[column_name_2] - df[column_name_1]
@@ -102,8 +112,15 @@ def transform_difference_ast_alt(dataframe, column_name_1, column_name_2, column
     dataframe = dataframe.drop([column_name], axis=1)
 
     return dataframe
+"""
+df.loc[df['proc_dif'] < 0, 'proc_dif_artmis?'] = 0
+df.loc[df['proc_dif'] >= 0, 'proc_dif_artmis?'] = 1
 
+df.loc[df['proc_dif'] < 0, 'proc_dif_azalmis?'] = 1
+df.loc[df['proc_dif'] >= 0, 'proc_dif_azalmis?'] = 0
+"""
 # print(df)
+
 df = transform_difference(df, "proc1", "proc2", "proc_diff")
 df = transform_difference(df, "crp1", "crp2", "crp_diff")
 df = transform_difference_ast_alt(df, "alt1", "alt2", "alt_diff")
@@ -133,16 +150,7 @@ df[:] = np.nan_to_num(df)
 
 # Bağımlı değişken ve bağımsız değişkenleri belirle
 # df['ex_BPD'] = df['ex'] + df['BPD'] + df['rop']
-# X = df.drop(['ex', 'BPD','rop','bilissel6ay', 'hareket6ay','bilissel9ay','hareket9ay','bilissel12ay', 'hareket12ay', 'bilissel16ay', 'hareket16ay', 'bilissel20ay'], axis=1)
-"""
-for x in ["proc_diff_increase?", "proc_decrease?", "potasyumdüşüklüğü", "IKK", "ast_increase?", "ast_decrease?", "urikasit_increase?", "urikasit_decrease?"]:
-    if x in list(df.columns):
-        print("FOUND")
-    else:
-        print(f"NOT FOUND {x}")
-"""
-X = df[["proc_diff_increase?", "proc_diff_decrease?", "potasyumdüşüklüğü", "IKK", "ast_diff_increase?", "ast_diff_decrease?", "urikasit_diff_increase?", "urikasit_diff_decrease?"]]
-print(X)
+X = df.drop(['ex', 'BPD','rop','bilissel6ay', 'hareket6ay','bilissel9ay','hareket9ay','bilissel12ay', 'hareket12ay', 'bilissel16ay', 'hareket16ay', 'bilissel20ay'], axis=1)
 y = df['ex']
 
 # Veriyi eğitim ve test setlerine ayır
@@ -180,10 +188,67 @@ model = GradientBoostingClassifier()
 #model = MLPClassifier()
 
 # print(y_train)
+"""
+full_pipeline = ColumnTransformer([('cat', OneHotEncoder(handle_unknown='ignore'), cat_attribs)], remainder='passthrough')
 
+encoder = full_pipeline.fit(X_train)
+X_train = encoder.transform(X_train)
+"""
 model.fit(X_train, y_train)
 
-# print(X_test.columns)
+
+
+"""
+print("Çalışma dizini:", os.getcwd())
+
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", "model.pkl")  # Masaüstüne kaydet
+with open(desktop_path, 'wb') as file:
+    pickle.dump(model, file)
+print(f"Model şu dizine kaydedildi: {desktop_path}")
+"""
+
+
+
+"""
+# showing all trees with random forest method
+for i, tree in enumerate(model.estimators_):
+    plt.figure(figsize=(20, 10))
+    plot_tree(tree, filled=True, feature_names=X.columns, class_names=["0", "1"], fontsize=10)
+    plt.title(f"Decision Tree {i+1}")
+    plt.show()
+"""
+
+"""
+# Masaüstünde bir klasör oluştur
+desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+folder_path = os.path.join(desktop_path, 'gbc')
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+"""
+# showing all trees with gradient boosting classifier method
+"""
+# Eğitim veri setinizdeki özellik isimlerini alın
+feature_names = list(X_train.columns)
+
+# Tüm ağaçları görselleştirin
+for i, estimator in enumerate(model.estimators_):
+    plt.figure(figsize=(19, 10))
+    plot_tree(estimator[0], filled=True, feature_names=feature_names)  # feature_names parametresini ekleyin
+    plt.title(f"Tree {i + 1}")
+    plt.show()
+"""
+"""
+# çalışmadı değişken adlarını yazınca hocaya sor
+    # Ağacın görselini kaydet
+    file_path = os.path.join(folder_path, f"ağaç_{i + 1}.png")
+    plt.savefig(file_path)
+    plt.close()
+
+print("Tüm ağaç görselleri başarıyla kaydedildi.")
+"""
+
+
 # Modelin performansını değerlendir
 y_pred = model.predict(X_test)
 # print(y_test, y_pred)
@@ -210,6 +275,17 @@ print(classification_report(y_test, y_pred_rounded))
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Squared Error: {mse}')
 
+"""
+# k fold validation
+k_folds = KFold(n_splits=5)
+
+scores = cross_val_score(model, X, y, cv=k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+"""
+
 # Özellik önem sıralamasını al
 feature_importances = model.feature_importances_
 
@@ -223,7 +299,7 @@ sorted_feature_importance = sorted(feature_importance_dict.items(), key=lambda x
 print("\nDeğişkenlerin Önem Sıralaması:")
 for feature, importance in sorted_feature_importance:
     print(f"{feature}: {importance}")
-
+"""
 # SHAP
 shap.initjs()
 
@@ -240,7 +316,8 @@ plt.ylabel('Importance Ranking')
 plt.title('Variable Importance Ranking')
 plt.xticks(rotation=45, ha="right")
 plt.show()
-
+"""
+"""
 # Area under curve
 # ROC AUC skoru hesaplama kısmı
 auc_score = roc_auc_score(y_test, y_pred_rounded)
@@ -254,7 +331,9 @@ roc_auc = auc(fpr, tpr)
 X_Y_Spline = make_interp_spline(fpr, tpr, k=2)  # k=2 ile ikinci dereceden spline oluşturuyoruz
 fpr_smooth = np.linspace(0, 1, 1000)
 tpr_smooth = X_Y_Spline(fpr_smooth)
+"""
 
+""""
 # ROC eğrisi çizilen kısım
 plt.figure()
 plt.plot(fpr_smooth, tpr_smooth , color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -267,14 +346,15 @@ plt.title('Receiver Operating Characteristic curve (ROC)')
 plt.legend(loc="lower right")
 plt.show()
 
-# Modeli kaydedeceğiniz klasörü belirtin
-model_dir = 'C:/Users/hasan/Desktop/model_save'
-if not os.path.exists(model_dir):
-    os.makedirs(model_dir)
-#
+"""
 # Modeli kaydet
-model_path = os.path.join(model_dir, 'model.pkl')
-joblib.dump(model, model_path)
+model_path = "trained_model.pkl"  # Kaydedilecek dosyanın adı
+with open(model_path, 'wb') as file:
+    pickle.dump(model, file)
 
-print(f"Model saved to {model_path}")
+print(f"Model başarıyla '{model_path}' olarak kaydedildi.")
 
+# Kullanılan sütunları terminale yazdır
+print("\nEğitimde Kullanılan Sütunlar:")
+for column in X.columns:
+    print(column)
